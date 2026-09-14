6470a8e78a3e9d745595638c78efd2e47d47b594
Damien Labbe <damien.labbe@fms-ea.com>
2026-09-14 13:46:42 +0200

    ajout de CREATE TABLE PERSON

--- bdd/prix_goncourt.sql
+++ bdd/prix_goncourt.sql
@@ -1,3 +1,12 @@
 DROP DATABASE IF EXISTS `prix_goncourt`;
 CREATE DATABASE IF NOT EXISTS `prix_goncourt` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE `prix_goncourt`;
+-- --------------------------------------------------------
+-- Table `PERSON`
+-- --------------------------------------------------------
+CREATE TABLE PERSON(
+   person_id INT NOT NULL PRIMARY KEY,
+   person_name VARCHAR(20) NOT NULL,
+   person_lastname VARCHAR(20) NOT NULL,
+   biography BLOB
+);
