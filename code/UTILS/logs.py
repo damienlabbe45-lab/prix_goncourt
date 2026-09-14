efb71660adf7ddeb2797e4b5b103003ab0472c57
Damien Labbe <damien.labbe@fms-ea.com>
2026-09-14 15:51:29 +0200

    ajout de la méthode formaTime

--- code/UTILS/logs.py
+++ code/UTILS/logs.py
@@ -4,3 +4,17 @@ from typing import Self
 
 class FormatDate(Formatter):
     """Formateur personnalisé pour utiliser la fonction date() pour l'horodatage."""
+
+    def formatTime(self: Self, record, datefmt=None):
+        """
+        Retourne la chaîne de date/heure personnalisée.
+        """
+        from datetime import datetime
+        maintenant = datetime.now()
+        return maintenant.strftime("%d %B %Y à %H heure %M minutes %S secondes %f microsecondes").replace(
+            m := maintenant.strftime("%B"),
+            {'January': 'janvier', 'February': 'février', 'March': 'mars', 'April': 'avril', 'May': 'mai',
+             'June': 'juin',
+             'July': 'juillet', 'August': 'août', 'September': 'septembre', 'October': 'octobre',
+             'November': 'novembre', 'December': 'décembre'}[m])
+
