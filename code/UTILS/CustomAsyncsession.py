8c4d2f487bf9cf117e394011784bd1dc10cbbed2
Damien Labbe <damien.labbe@fms-ea.com>
2026-09-14 15:39:32 +0200

    créations des lignes d'importations et de la classe CustomAsyncSession

---
+++ code/UTILS/CustomAsyncsession.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from typing import Any
+from sqlalchemy import text
+from sqlalchemy.engine import Result
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+class CustomAsyncSession(AsyncSession):
+    """Session asynchrone personnalisée intégrant le wrapper text() et les méthodes utilitaires."""
