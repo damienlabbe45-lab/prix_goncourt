5198054939c1109b24fb0758f9fdcd43af18dea3
Damien Labbe <damien.labbe@fms-ea.com>
2026-09-14 15:40:25 +0200

    ajout de la méthode _text

--- code/UTILS/CustomAsyncsession.py
+++ code/UTILS/CustomAsyncsession.py
@@ -8,3 +8,6 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 class CustomAsyncSession(AsyncSession):
     """Session asynchrone personnalisée intégrant le wrapper text() et les méthodes utilitaires."""
+
+    def _text(self, statement: Any) -> Any:
+        return text(statement) if isinstance(statement, str) else statement
