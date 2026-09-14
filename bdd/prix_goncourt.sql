a5418b640712cae13ef8efa8b55c01e19d108a4c
Damien Labbe <damien.labbe@fms-ea.com>
2026-09-14 13:57:35 +0200

    ajout de CREATE TABLE JURY_MEMBER

--- bdd/prix_goncourt.sql
+++ bdd/prix_goncourt.sql
@@ -10,3 +10,14 @@ CREATE TABLE PERSON(
    person_lastname VARCHAR(20) NOT NULL,
    biography BLOB
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+-- Table `JURY_MEMBER`
+-- --------------------------------------------------------
+
+CREATE TABLE JURY_MEMBER(
+   person_id INT NOT NULL UNIQUE,
+   chairman BOOLEAN,
+   member_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+   CONSTRAINT "fk_person_jury" FOREIGN KEY (person_id) REFERENCES PERSON(person_id)
+   )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
