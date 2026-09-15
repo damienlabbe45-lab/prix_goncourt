2a0da20e1eb61718d407b58ce0a52747427ed7c7
Damien Labbe <damien.labbe@fms-ea.com>
2026-09-15 13:06:10 +0200

    ajout de override sur read et read_all

--- code/DAO/dao_character.py
+++ code/DAO/dao_character.py
@@ -5,7 +5,7 @@ Classe Dao[Character]
 """
 
 from dataclasses import dataclass
-from typing import Optional
+from typing import Optional, override
 from modeles.character import Character
 from dao.dao import Dao
 
@@ -19,6 +19,7 @@ class CharacterDao(Dao[Character]):
 
         return character
 
+    @override(Dao)
     async def read(self, id_entity: int) -> Optional[Character]:
         """Renvoit le personnage correspondant à l'entité dont l'id est id_entity
            (ou None s'il n'a pu être trouvé)"""
@@ -28,6 +29,7 @@ class CharacterDao(Dao[Character]):
                                             {"c": id_entity})).fetchone()
             return self.character_from_db(record) if record is not None else None
 
+    @override(Dao)
     async def read_all(self) -> list[Character]:
         """Renvoit l'ensemble des personnages principaux de la BD."""
         character_list: list[Character] = []
