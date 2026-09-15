a9de960ac53be848a2f829d673422a1f56677add
Damien Labbe <damien.labbe@fms-ea.com>
2026-09-15 13:19:22 +0200

    ajout de la méthode read

--- code/DAO/dao_author.py
+++ code/DAO/dao_author.py
@@ -17,3 +17,13 @@ class AuthorDao(Dao[Author]):
     def author_from_db(record: tuple[str, str, str]) -> Author:
         """Construit un auteur du modèle d'après son entité en BD"""
         return Author(record[0], record[1], record[2])
+
+    @override(Dao)
+    async def read(self, id_entity: int) -> Optional[Author]:
+        """Renvoit le personnage correspondant à l'entité dont l'id est id_entity
+           (ou None s'il n'a pu être trouvé)"""
+        async with self.connection() as session:
+            record = (await session.execute("""SELECT perso_name, person_lastname, biography FROM person WHERE 
+                person_id = (select person_id from AUTHOR where author_id = :c)""",
+                                            {"c": id_entity})).fetchone()
+            return self.author_from_db(record) if record is not None else None
