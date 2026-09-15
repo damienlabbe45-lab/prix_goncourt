49ab4358704066ec2e0da3283bf7cb45fd4920b9
Damien Labbe <damien.labbe@fms-ea.com>
2026-09-15 13:26:39 +0200

    ajout de la méthode read

--- code/DAO/dao_jurymember.py
+++ code/DAO/dao_jurymember.py
@@ -18,3 +18,13 @@ class JuryMemberDao(Dao[JuryMember]):
         """Construit un auteur du modèle d'après son entité en BD"""
         return JuryMember(record[0], record[1], record[2], record[3])
 
+    @override(Dao)
+    async def read(self, id_entity: int) -> Optional[JuryMember]:
+        """Renvoit le personnage correspondant à l'entité dont l'id est id_entity
+           (ou None s'il n'a pu être trouvé)"""
+        async with self.connection() as session:
+            record = (await session.execute("""SELECT perso_name, person_lastname, biography FROM person WHERE 
+                person_id = (select person_id from AUTHOR where author_id = :c)""",
+                                            {"c": id_entity})).fetchone()
+            return self.jury_from_db(record) if record is not None else None
+
