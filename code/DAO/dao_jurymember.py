7af0f2100616d266d8eb27da3eb3368dcf0f7a4c
Damien Labbe <damien.labbe@fms-ea.com>
2026-09-15 13:35:52 +0200

    modification de la requête sql dans read

--- code/DAO/dao_jurymember.py
+++ code/DAO/dao_jurymember.py
@@ -23,9 +23,9 @@ class JuryMemberDao(Dao[JuryMember]):
         """Renvoit le personnage correspondant à l'entité dont l'id est id_entity
            (ou None s'il n'a pu être trouvé)"""
         async with self.connection() as session:
-            record = (await session.execute("""SELECT perso_name, person_lastname, biography FROM person WHERE 
-                person_id = (select person_id from AUTHOR where author_id = :c)""",
-                                            {"c": id_entity})).fetchone()
+            record = (await session.execute("""SELECT perso_name, person_lastname, biography, chairman FROM person 
+            OIN JURY_MEMBER ON person.person_id = JURY_MEMBER.person_id WHERE member_id = :c""", {"c": id_entity})
+                      ).fetchone()
             return self.jury_from_db(record) if record is not None else None
 
     @override(Dao)
