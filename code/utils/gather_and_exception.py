c8f9e2bd193b011b8d896b431affbec2285e30fa
Damien Labbe <damien.labbe@fms-ea.com>
2026-09-15 15:14:10 +0200

    ajout de la fonction gather_exceptions

--- code/utils/gather_and_exception.py
+++ code/utils/gather_and_exception.py
@@ -3,7 +3,6 @@ from collections.abc import Awaitable
 from asyncio import gather
 
 
-
 def manage_exception(exceptions: list[Any]) -> list[Any]:
     """gère toutes les exceptions lors d'éxécutions en parallèle"""
     exceptionss = [r for r in exceptions if isinstance(r, BaseException)]
@@ -13,3 +12,8 @@ def manage_exception(exceptions: list[Any]) -> list[Any]:
                 "une ou plusieurs exceptions se sont produites", exceptionss)
         raise exceptionss[0]
     return exceptions
+
+
+async def gather_exceptions(*coros: Awaitable[Any]) -> list[Any]:
+    """gere les exceptions de gather de manière générale et éxécute les coroutines en parralèle"""
+    return manage_exception(await gather(*coros, return_exceptions=True))
