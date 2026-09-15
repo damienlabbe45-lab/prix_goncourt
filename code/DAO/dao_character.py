1d0f7dd004468abcdffc118a3ea6d3680b6d2b7c
Damien Labbe <damien.labbe@fms-ea.com>
2026-09-15 12:02:49 +0200

    ajout de la méthode character_from_db

--- code/DAO/dao_character.py
+++ code/DAO/dao_character.py
@@ -12,5 +12,12 @@ from dao.dao import Dao
 
 @dataclass
 class CharacterDao(Dao[Character]):
+    @staticmethod
+    def character_from_db(record) -> Character:
+        """Construit un cours du modèle d'après son entité en BD"""
+        character: Character = Character(record[0], record[1], record[2])
+
+        return character
+
 
 
