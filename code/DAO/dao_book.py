8ba39b8406ebb0ca2b3579839c8262f8503286c8
Damien Labbe <damien.labbe@fms-ea.com>
2026-09-15 15:50:51 +0200

    ajout de la classe BookDao

---
+++ code/DAO/dao_book.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+"""
+Classe Dao[Book]
+"""
+
+from dataclasses import dataclass
+from datetime import date
+from typing import Optional, override
+from modeles.book import Book
+from dao.dao import Dao
+from dao.dao_author import AuthorDao
+from modeles.author import Author
+from dao.dao_character import CharacterDao
+
+
+@dataclass
+class BookDao(Dao[Book]):
+
