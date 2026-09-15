2afb64df6dd8be554d6216b5c9e7b64fce76934a
Damien Labbe <damien.labbe@fms-ea.com>
2026-09-15 15:49:09 +0200

    suppression de l'héritage à ABC

--- code/modeles/selection.py
+++ code/modeles/selection.py
@@ -6,21 +6,20 @@ Classe Selection
 
 from dataclasses import dataclass, field
 from typing import override
-from abc import ABC
 from datetime import date
 from lIteraryprize import LiteraryPrize
 from book import Book
 
 
 @dataclass
-class Selection(ABC):
+class Selection:
     """Selection des livres par les prix littéraires ainsi que le nombre de vote pour chaque livre"""
     date_selection: date
     name_prize: LiteraryPrize
     selection: int
     dic_book_vote: dict[str, int] = field(default_factory=dict)
 
-    @override(ABC)
+    @override
     def __str__(self) -> str:
         list_results: list[str] = [f"{key}: {values} votes" for key, values in self.dic_book_vote.items()]
         return f""" la sélection numéro {self.selection} du prix littéraire {self.name_prize} a lieu le 
