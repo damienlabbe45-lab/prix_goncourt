dfd7c5a8c9038d2419d2004b5617528979ba54a1
Damien Labbe <damien.labbe@fms-ea.com>
2026-09-16 15:02:53 +0200

    suppression des () et de à l'interieur de celles ci dans override

--- code/modeles/person.py
+++ code/modeles/person.py
@@ -16,7 +16,7 @@ class Person(ABC):
     last_name: str
     biography: str | None
 
-    @override(ABC)
+    @override
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name} " + \
                (f", {self.biography}" if self.biography is not None else '')
