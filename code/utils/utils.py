409766d949b4facaf1431f1aca1648bd09b03699
Damien Labbe <damien.labbe@fms-ea.com>
2026-09-16 14:58:01 +0200

    rename de gather_and_exception.py en utils.py et ajout de la fonction execute_insert

--- code/utils/utils.py
+++ code/utils/utils.py
@@ -1,6 +1,7 @@
 from typing import Any
 from collections.abc import Awaitable
 from asyncio import gather
+from dao.dao_selection import SelectionDao
 
 
 def manage_exception(exceptions: list[Any]) -> list[Any]:
@@ -17,3 +18,8 @@ def manage_exception(exceptions: list[Any]) -> list[Any]:
 async def gather_exceptions(*coros: Awaitable[Any]) -> list[Any]:
     """gere les exceptions de gather de manière générale et éxécute les coroutines en parralèle"""
     return manage_exception(await gather(*coros, return_exceptions=True))
+
+
+async def execute_insert(query: str, params: list[dict] | dict) -> None:
+    async with SelectionDao.connection() as session, session.begin():
+        await session.execute(query, params)
