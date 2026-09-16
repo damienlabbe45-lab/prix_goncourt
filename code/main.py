3a3a5c71e093ad38e81bfcc19d7727b56cd64612
Damien Labbe <damien.labbe@fms-ea.com>
2026-09-16 15:32:25 +0200

    changement de la fonction main pour appeler create_table

--- code/main.py
+++ code/main.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 from asyncio import run
 from test_data.insert_data import insert_data
+from test_data.sql_create_table import create_table
 
 """
 Application des prix littéraires
@@ -10,6 +11,7 @@ Application des prix littéraires
 
 async def main() -> None:
     """Programme principal."""
+    await create_table()
     await insert_data()
 
 if __name__ == '__main__':
