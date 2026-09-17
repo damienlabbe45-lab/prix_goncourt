13a53bfff41b437e3845ac92af192882678f5d5e
Damien Labbe <damien.labbe@fms-ea.com>
2026-09-17 13:27:27 +0200

    création de la fonction vote_book

--- code/interface/charman_jury_interface.py
+++ code/interface/charman_jury_interface.py
@@ -17,3 +17,27 @@ class CharmanJuryInterface(VisitorInterface):
         response = input_selection(counter, id_book)
         return books[response], response
 
+    async def vote_book(self, select: int):
+        number = int(len(await self.selection_book(select)) / 2)
+        counter = len(await JuryMemberDao().read_all())
+        book = []
+        vote = []
+        list_id_book = []
+        for _ in range(number):
+            result = await self.selection_book_jury(select, list_id_book)
+            book.append(result[0])
+            list_id_book.append(result[1])
+            vote.append(input_selection(counter))
+
+        for _ in range(number):
+            vote.append(0)
+
+        params = []
+        number = 0
+        selection_dao = SelectionDao()
+        isbn_id = await selection_dao.mapping_isbn_book_id(select)
+        while len(params) < len(book):
+            books = book[number]
+            isbn = search(r"Son numéro isbc est: ([\d-]+)", books).group(1)
+            params.append({"b": isbn_id[isbn], "v": vote[number], "s": select})
+        await selection_dao.insert_by_member(params, select)
