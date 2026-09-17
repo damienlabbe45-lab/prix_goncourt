493b5561fd5e09949df95413d48402c9e24528f5
Damien Labbe <damien.labbe@fms-ea.com>
2026-09-17 13:25:13 +0200

    création de la classe CharmanJuryinterface

---
+++ code/interface/charman_jury_interface.py
@@ -0,0 +1,9 @@
+from interface.visitor_interface import VisitorInterface
+from dao.dao_selection import SelectionDao
+from utils.utils import input_selection
+from dao.dao_jurymember import JuryMemberDao
+from re import search
+
+
+class CharmanJuryInterface(VisitorInterface):
+
