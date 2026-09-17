a69d883f30295726ae6960f1b7d0817e6fb661c5
Damien Labbe <damien.labbe@fms-ea.com>
2026-09-17 13:26:07 +0200

    création de la fonction selection_book_jury

--- code/interface/charman_jury_interface.py
+++ code/interface/charman_jury_interface.py
@@ -6,4 +6,14 @@ from re import search
 
 
 class CharmanJuryInterface(VisitorInterface):
+    async def selection_book_jury(self, select: int, id_book: list[int]) -> tuple[str, int]:
+        read_selection_dao = await SelectionDao().read(select)
+        counter = 0
+        books = list(read_selection_dao.dic_book_vote.keys())
+        for book in books:
+            if book not in id_book:
+                print(f"pour voter le livre {book} , taper {counter}")
+            counter += 1
+        response = input_selection(counter, id_book)
+        return books[response], response
 
