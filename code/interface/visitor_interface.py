7bd696ea4d0a27a51fc693f14ab5bec106178029
Damien Labbe <damien.labbe@fms-ea.com>
2026-09-17 13:23:09 +0200

    création de la fonction selection_book

--- code/interface/visitor_interface.py
+++ code/interface/visitor_interface.py
@@ -2,4 +2,6 @@ from dao.dao_selection import SelectionDao
 
 
 class VisitorInterface:
-
+    async def selection_book(self, select: int):
+        read_selection_dao = await SelectionDao().read(select)
+        return list(read_selection_dao.dic_book_vote.keys())
