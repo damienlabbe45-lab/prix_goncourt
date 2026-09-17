4ad11cfcc6a3bf4c575e157f6b94afe2d6f4a228
Damien Labbe <damien.labbe@fms-ea.com>
2026-09-17 15:18:35 +0200

    utilisation du logging personalisé et de la fonction selection_charman_initial

--- code/main.py
+++ code/main.py
@@ -3,6 +3,8 @@
 from asyncio import run
 from test_data.insert_data import insert_data
 from test_data.sql_create_table import create_table
+from interface.charman_jury_interface import CharmanJuryInterface
+from utils.logs import LogErreur
 
 """
 Application des prix littéraires
@@ -11,8 +13,10 @@ Application des prix littéraires
 
 async def main() -> None:
     """Programme principal."""
+    LogErreur().loggingperso()
     await create_table()
     await insert_data()
+    await CharmanJuryInterface().selection_charman_initial()
 
 if __name__ == '__main__':
     run(main())
