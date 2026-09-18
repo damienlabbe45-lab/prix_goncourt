f3fb382eb7f4f3070995a94d9209d02c16808737
Damien Labbe <damien.labbe@fms-ea.com>
2026-09-18 13:20:50 +0200

    la méthode selection_book de VisitorInterface a été mise pour JuryMemberInterface à laplace

--- code/interface/jury_member_interface.py
+++ code/interface/jury_member_interface.py
@@ -1,5 +1,8 @@
 from interface.visitor_interface import VisitorInterface
+from dao.dao_selection import SelectionDao
 
 
 class JuryMemberInterface(VisitorInterface):
-    pass
+    async def selection_book(self, select: int):
+        read_selection_dao = await SelectionDao().read(select)
+        return list(read_selection_dao.dic_book_vote.keys())
--- code/interface/visitor_interface.py
+++ code/interface/visitor_interface.py
@@ -2,6 +2,4 @@ from dao.dao_selection import SelectionDao
 
 
 class VisitorInterface:
-    async def selection_book(self, select: int):
-        read_selection_dao = await SelectionDao().read(select)
-        return list(read_selection_dao.dic_book_vote.keys())
+    pass
